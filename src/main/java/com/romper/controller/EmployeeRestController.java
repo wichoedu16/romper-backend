d32bc41cec4ae323fb7e6da189626932f0e23ff7
wichoedu16 <wichoedu16@gmail.com>
2024-05-15 20:23:25 -0500

    modify controller add cors annotation

--- src/main/java/com/romper/controller/EmployeeRestController.java
+++ src/main/java/com/romper/controller/EmployeeRestController.java
@@ -7,61 +7,65 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeRestController {
+
+    private final IEmployeeService employeeService;
+
     @Autowired
-    private IEmployeeService employeeService;
+    public EmployeeRestController(IEmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
     /**
      * Get all employees
-     * @return List<Employees>
+     * @return List of Employees
      */
     @GetMapping("/employees")
-    public ResponseEntity<EmployeeResponseRest> searchEmployees(){
-        ResponseEntity<EmployeeResponseRest> response = employeeService.search();
-        return response;
+    public ResponseEntity<EmployeeResponseRest> searchEmployees() {
+        return employeeService.search();
     }
 
     /**
      * Get an employee by id
-     * @param id
+     * @param id Employee ID
      * @return Employee
      */
     @GetMapping("/employees/{id}")
-    public ResponseEntity<EmployeeResponseRest> searchEmployeesById(@PathVariable Long id){
-        ResponseEntity<EmployeeResponseRest> response = employeeService.searchById(id);
-        return response;
+    public ResponseEntity<EmployeeResponseRest> searchEmployeesById(@PathVariable Long id) {
+        return employeeService.searchById(id);
     }
 
     /**
      * Save employee
-     * @param employee
+     * @param employee Employee object
+     * @return Saved Employee
      */
     @PostMapping("/employees")
-    public ResponseEntity<EmployeeResponseRest> save(@RequestBody Employee employee){
-        ResponseEntity<EmployeeResponseRest> response = employeeService.save(employee);
-        return response;
+    public ResponseEntity<EmployeeResponseRest> save(@RequestBody Employee employee) {
+        return employeeService.save(employee);
     }
 
     /**
      * Update employee
-     * @param employee
-     * @param id
+     * @param employee Employee object
+     * @param id Employee ID
+     * @return Updated Employee
      */
     @PutMapping("/employees/{id}")
-    public ResponseEntity<EmployeeResponseRest> update(@RequestBody Employee employee, @PathVariable Long id){
-        ResponseEntity<EmployeeResponseRest> response = employeeService.update(employee, id);
-        return response;
+    public ResponseEntity<EmployeeResponseRest> update(@RequestBody Employee employee, @PathVariable Long id) {
+        return employeeService.update(employee, id);
     }
 
     /**
      * Delete employee by Id
-     * @param id
+     * @param id Employee ID
+     * @return Deleted Employee
      */
     @DeleteMapping("/employees/{id}")
-    public ResponseEntity<EmployeeResponseRest> delete(@PathVariable Long id){
-        ResponseEntity<EmployeeResponseRest> response = employeeService.deleteById(id);
-        return response;
+    public ResponseEntity<EmployeeResponseRest> delete(@PathVariable Long id) {
+        return employeeService.deleteById(id);
     }
 }
